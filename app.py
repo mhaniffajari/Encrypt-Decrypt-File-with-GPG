c7ef48ba6bb0efaf708812ce53e4b622c3b4165f
Muhammad Hanif Fajari <105597377+mhaniffajari@users.noreply.github.com>
2024-10-10 19:23:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ gpg = gnupg.GPG()
 
 # Function to generate and export a new GPG key
 def generate_key():
-    input_data = gpg.gen_key_input(name_email="user@example.com", passphrase="your-passphrase")
+    input_data = gpg.gen_key_input(name_email="user@example.com", passphrase="p@ssphr@se167?")
     key = gpg.gen_key(input_data)
     
     # Export the private and public keys
     public_key = gpg.export_keys(key.fingerprint)
-    private_key = gpg.export_keys(key.fingerprint, secret=True, passphrase="your-passphrase")
+    private_key = gpg.export_keys(key.fingerprint, secret=True, passphrase="p@ssphr@se167?")
     
     # Save both keys to files
     with open('public_key.asc', 'w') as pub_file:
